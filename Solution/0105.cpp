866af0f3728d865a4a10833b6a3d7ddc293980cf
alun <come880412@gmail.com>
2024-06-09 02:00:09 +0800

    0105 cpp

--- Solution/0105.cpp
+++ Solution/0105.cpp
@@ -11,27 +11,26 @@
  */
 class Solution {
 public:
-    map<int, int> inorder_m;
-    TreeNode* build(vector<int>& inorder, vector<int>& preorder, 
-                    int s_in, int e_in, int s_pre, int e_pre){
-        if(s_in > e_in || s_pre > e_pre)
-            return NULL;
-        else{
-            TreeNode* root = new TreeNode(preorder[s_pre]);
-            int root_idx = inorder_m[preorder[s_pre]];
-            int left_num = root_idx - s_in;
+    TreeNode* Traverse(vector<int> &preorder, vector<int> &inorder) {
+        int preorderVal = preorder[0];
 
-            root -> left = build(inorder, preorder, s_in, root_idx - 1, s_pre + 1, s_pre + left_num);
-            root -> right = build(inorder, preorder, root_idx + 1, e_in, s_pre + left_num + 1, e_pre);
-            
+        vector<int>::iterator itr = find(inorder.begin(), inorder.end(), preorderVal);
+        if (itr == inorder.end()) return nullptr; // not find the value in the inorder vector.
+        else {
+            vector<int> leftTreeInorder(inorder.begin(), itr);
+            vector<int> rightTreeInorder(itr + 1, inorder.end());
+
+            TreeNode *root = new TreeNode(preorderVal);
+            preorder.erase(preorder.begin());
+            root -> left = Traverse(preorder, leftTreeInorder);
+            root -> right = Traverse(preorder, rightTreeInorder);
             return root;
         }
     }
 
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        for(int i=0; i < inorder.size(); ++i)
-            inorder_m[inorder[i]] = i;
-        
-        return build(inorder, preorder, 0, inorder.size() - 1, 0, preorder.size() - 1);
+        TreeNode *root;
+        root = Traverse(preorder, inorder);
+        return root;
     }
 };
