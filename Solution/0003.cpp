25f05087eb02ddb595a40c4d3e802af28d60777b
alun <come880412@gmail.com>
2024-06-16 00:26:02 +0800

    0003 cpp

--- Solution/0003.cpp
+++ Solution/0003.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        map<char, int> charDict;
-        
-        int maxLen = 0, start=0, end=0;
-        for(int i=0 ; i < s.size() ; ++i){
-            if(charDict.find(s[i]) != charDict.end()){
-                end = charDict[s[i]];
-                for(int j=start; j<=end; ++j){
-                    charDict.erase(s[j]);
-                }
-                start = end + 1;
+        int n = s.length();
+
+        unordered_map<char, int> map;
+        int prev = -1;
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            if (map.find(s[i]) != map.end() && map[s[i]] > prev) {
+                prev = map[s[i]];
             }
-            charDict[s[i]] = i;
-            
-            if(charDict.size() > maxLen) maxLen = charDict.size();
+            map[s[i]] = i;
+            result = max(result, i - prev);
         }
-        
-        return maxLen;
+        return result;
     }
 };
