cc01e3a68a98858225b893e392be1d06cf9cf68b
GiLuenHuang <GiLuenHuang@mobiledrivetech.com>
2024-06-24 17:58:08 +0800

    0238 cpp

--- Solution/0238.cpp
+++ Solution/0238.cpp
@@ -1,46 +1,25 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> answer;
+        int n = nums.size();
+        vector<int> ans(n, 0);
         
-        bool double_zeros=false;
-        bool single_zero=false;
-        int movingMul=1;
-        
-        for(int i=0 ; i<nums.size() ; ++i){
-            if(nums[i] == 0){
-                if(single_zero){
-                    double_zeros = true;
-                    break;
-                }
-                single_zero = true;
-            }
-            
-            else{
-                movingMul *= nums[i];
-            }
+        int totalMul = 1;
+        int cnt = 0;
+        for (int x : nums) {
+            if (x != 0) totalMul *= x;
+            else cnt += 1;
         }
         
-        for(int i=0 ; i<nums.size() ; ++i){
-            if(double_zeros)
-                answer.push_back(0);
-            
-            else{
-                
-                if(single_zero){
-                    if(nums[i] == 0)
-                        answer.push_back(movingMul);
-                    else
-                        answer.push_back(0);
-                }
-                
-                else{
-                    answer.push_back(movingMul / nums[i]);
-                }
-                
-            }
+        for (int i = n - 1; i >= 0; --i) {
+            if (cnt >= 2) 
+                ans[i] = 0;
+            else if (cnt == 1) {
+                int val = (nums[i] == 0) ? totalMul : 0;
+                ans[i] = val;
+            } else 
+                ans[i] = totalMul / nums[i];
         }
-        
-        return answer;
+        return ans;
     }
 };
