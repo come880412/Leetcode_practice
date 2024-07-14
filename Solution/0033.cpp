8206bfa8804414e0ffdbe6cc2581bdd7eb9123bf
GiLuenHuang <GiLuenHuang@mobiledrivetech.com>
2024-07-15 01:13:24 +0800

    0033 cpp

--- Solution/0033.cpp
+++ Solution/0033.cpp
@@ -4,28 +4,18 @@ public:
         int s = 0;
         int e = nums.size() - 1;
         
-        int m;
-        
-        while(s <= e){
-            m = floor((s + e) / 2);
+        while (s <= e) {
+            int m = (s + e) / 2;
             
-            if(nums[m] == target)
-                return m;
-            
-            if(nums[m] < nums[s]){
-                if(target > nums[m] && nums[e] >= target) s = m + 1;
+            if (nums[m] == target) return m;
+            else if (nums[m] > target) {
+                if (target < nums[s] && nums[m] >= nums[s]) s = m + 1;
                 else e = m - 1;
-            }
-            
-            else{
-                if(target < nums[m] && nums[s] <= target) e = m - 1;
+            } else {
+                if (target > nums[e] && nums[m] < nums[s]) e = m - 1;
                 else s = m + 1;
             }
-            
-            
         }
-        
         return -1;
-        
     }
 };
