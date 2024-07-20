1b5f69115f353ec88bc052640bb9340d925f9b68
GiLuenHuang <GiLuenHuang@mobiledrivetech.com>
2024-07-20 15:22:14 +0800

    0056 cpp

--- Solution/0056.cpp
+++ Solution/0056.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        
-        int end, idx = 0;
-        end = intervals[idx][1];
-        for(int i=1 ; i<intervals.size() ; ++i){
-            // Merge
-            if(intervals[i][0] <= end){
-                end = max(intervals[i][1], end);
+
+        int end = intervals[0][1], idx = 0;
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i][0] <= end) {
+                end = max(end, intervals[i][1]);
                 intervals[idx][1] = end;
-            }
-
-            // Save result
-            else{
+            } else {
                 ++idx;
-                intervals[idx][0] = intervals[i][0]; 
+                intervals[idx][0] = intervals[i][0];
                 intervals[idx][1] = intervals[i][1];
-                end = intervals[i][1];
+                end = intervals[idx][1];
             }
-
         }
-        intervals.erase(intervals.begin()+idx+1, intervals.end());
-
+        intervals.erase(intervals.begin() + idx + 1, intervals.end());
         return intervals;
     }
 };
