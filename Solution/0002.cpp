2bb6d461152c46659d06dd29533704edc405feb6
GiLuenHuang <GiLuenHuang@mobiledrivetech.com>
2024-08-02 22:04:42 +0800

    0002 cpp

--- Solution/0002.cpp
+++ Solution/0002.cpp
@@ -11,25 +11,19 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* result = new ListNode(-1);
-        ListNode* cur = result;
-        int sum = 0, carry=0;
-        while (l1 != nullptr || l2 != nullptr) {
-            int l1_num = l1 != nullptr ? l1->val : 0;
-            int l2_num = l2 != nullptr ? l2->val : 0;
-            sum = (l1_num + l2_num + carry) % 10;
-            carry = (l1_num + l2_num + carry) / 10;
+        ListNode dummy = ListNode();
+        ListNode *cur = &dummy;
+        int sum = 0, carry = 0;
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int val1 = l1 == nullptr ? 0 : l1 -> val;
+            int val2 = l2 == nullptr ? 0 : l2 -> val;
+            sum = (val1 + val2 + carry) % 10;
+            carry = (val1 + val2 + carry) / 10;
+            l1 = l1 == nullptr ? l1 : l1 -> next;
+            l2 = l2 == nullptr ? l2 : l2 -> next;
             cur -> next = new ListNode(sum);
             cur = cur -> next;
-            if (l1 != nullptr)
-                l1 = l1 -> next;
-            if (l2 != nullptr)
-                l2 = l2 -> next;
-            
         }
-        if (carry)
-            cur -> next = new ListNode(carry);
-        
-        return result->next;
+        return dummy.next;
     }
 };
