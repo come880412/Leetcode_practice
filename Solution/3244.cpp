2c28e08dd30e8a3b1481491f9431dab0bbc0204f
GiLuenHuang <GiLuenHuang@mobiledrivetech.com>
2024-08-05 02:04:17 +0800

    3244 cpp

--- Solution/3244.cpp
+++ Solution/3244.cpp
@@ -11,9 +11,9 @@ public:
             // After each query, the nodes which lies between ith Query Edge will disappear after ith Query
             // 0-1-2-3-4   ===> Now, Suppose Add edge (1,4)  
             // ===> Then Shortest path will be 0-1-4 (or nodes 2 and 3 disappear)
-            auto it_start = s.lower_bound(fromCity + 1);
-            auto it_end = s.upper_bound(toCity - 1);
-            s.erase(it_start, it_end);
+            auto it_start = s.lower_bound(fromCity + 1); // Find the value greater than or equal to fromCity + 1
+            auto it_end = s.upper_bound(toCity - 1);     // Find the value greater than toCity - 1  
+            s.erase(it_start, it_end); // Erase the value in range [it_start, it_end)
             
             result.push_back(s.size() - 1);
         }
