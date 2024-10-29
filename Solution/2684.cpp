bdec5b2b500af4f9d7f437892cbe5e0ec25dbd8f
alun <come880412@gmail.com>
2024-10-30 00:45:26 +0800

    2684 cpp

--- Solution/2684.cpp
+++ Solution/2684.cpp
@@ -1,3 +1,4 @@
+/*BFS solution
 class Solution {
 public:
     vector<pair<int, int>> dir{{-1, 1}, {0, 1}, {1, 1}};
@@ -31,6 +32,33 @@ public:
             }
             maxMove = max(maxMove, move);
         }
+        return maxMove;
+    }
+};
+*/
+
+// Dynamic programming (DP)
+class Solution {
+public:
+    vector<pair<int, int>> dir{{-1, 1}, {0, 1}, {1, 1}};
+    int maxMoves(vector<vector<int>>& grid) {
+        const int m = grid.size();
+        const int n = grid[0].size();
+        int maxMove = 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int c = n - 2; c >= 0; --c) {
+            for (int r = 0; r < m; ++r) {
+                for (const auto& [dr, dc] : dir) {
+                    int currR = r + dr, currC = c + dc;
+                    if (currR < 0 || currC < 0 || currR >= m || currC >= n) continue;
+                    if (grid[currR][currC] > grid[r][c])
+                        dp[r][c] = max(dp[r][c], dp[currR][currC] + 1);
+                }
+            }
+        }
+        for (int r = 0; r < m; ++r) 
+            maxMove = max(maxMove, dp[r][0]);
         return maxMove;
     }
 };
