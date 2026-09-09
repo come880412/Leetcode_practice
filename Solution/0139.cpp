2819d7adfcb59909b4870fd6432c86a7996b096a
alun <come880412@gmail.com>
2026-09-10 01:53:03 +0800

    0139 cpp solution update

--- Solution/0139.cpp
+++ Solution/0139.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        // To achieve O(1) searching time.
-        unordered_set <string> wordHashSet(wordDict.begin(), wordDict.end());
-        vector <bool> record(s.length() + 1, false);
-        
-        record[0] = true;
-        for (int i=0; i<s.length(); ++i) {
-            for (int j=0; j<=i; ++j) {
-                string subStr = s.substr(j, i-j+1);
-                if(record[j] && wordHashSet.count(subStr)) {
-                    record[i+1] = true;
+        int n = s.length();
+        unordered_set <string> wordSet(wordDict.begin(), wordDict.end());
+        vector<bool> dp(n+1, false);
+
+        dp[0] = true;
+        for (int i = 1; i < n + 1; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] && wordSet.find(s.substr(j, i-j)) != wordSet.end()) {
+                    dp[i] = true;
                     break;
                 }
             }
         }
-        
-        return record[s.length()];
+        return dp[n];
     }
 };
